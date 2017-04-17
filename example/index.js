6d107cd643862e4ad18a12c19d792cb8fa08f6b7
orthagonal <orthagonal1@gmail.com>
2017-04-17 14:59:26 -0500

    v1

--- example/index.js
+++ example/index.js
@@ -1,20 +1,25 @@
 const Hapi = require('hapi');
 const plugin = require('../');
-const server = new Hapi.Server({ debug: { log: ['ops and cpu', 'requests'] } });
+const server = new Hapi.Server({ debug: { log: ['ops', 'threshold'] } });
 server.connection({ port: 8080 });
 server.register({
   register: plugin,
   options: {
-    interval: 2000
+    interval: 5000, // log some data every 5 seconds
+    memoryThreshold: 40, // alert if memory use is above this threshold
+    cpuThresholds: [.5, .4, .3], // alert if 1/5/15 minute CPU averages are above their respective threshold
+    failedRequestThreshold: 10 // alert if the % of requests that don't return 200 is above this threshold
   }
 }, () => {
   server.route({
     path: '/',
     method: 'GET',
     handler(req, reply) {
-      console.log('route called');
       reply('hi');
     }
   });
-  server.start(() => {});
+  server.start(() => {
+    // try hitting the '/' route as well as an undefined route to see request data
+    // for both successful and failed HTTP transactions
+  });
 });
--- index.js
+++ index.js
@@ -3,17 +3,18 @@ const chalk = require('chalk');
 const Oppsy = require('oppsy');
 
 const defaults = {
-  cpu: true,
-  requests: true,
-  httpErrorRequestThreshold: false,
-  memoryThreshold: 20
+  failedRequestThreshold: 100,
+  memoryThreshold: 100,
+  cpuThresholds: [1.0, 1.0, 1.0] // 1, 5 and 15-minute load averages
 };
 
 exports.register = function(server, options, next) {
   const port = server.info.port;
-  options = Object.assign(defaults, options);
+  options = Object.assign({}, defaults, options);
   const interval = options.interval || 1000 * 15;
   const oppsy = new Oppsy(server);
+  let totalRequestsProcessed = 0;
+  let totalErrorRequests = 0;
   oppsy.on('ops', (data) => {
     // count # of HTTP errs per report:
     let httpErrorRequestCount = 0;
@@ -24,33 +25,53 @@ exports.register = function(server, options, next) {
         }
       });
     }
-
     // log cpu/mem stats:
-    const oneMb = 1048576;
-    const freeMb = data.osmem.free / oneMb;
-    const totalMb = data.osmem.total / oneMb;
+    const freeMb = data.osmem.free / 1048576;
+    const totalMb = data.osmem.total / 1048576;
     const memPercent = (freeMb / totalMb).toFixed(2) * 100;
     const memPercentString = chalk.blue(`${memPercent}%`);
-    server.log(['ops and cpu'], `
+    server.log(['ops', 'cpu'], `
     ${chalk.yellow('System Memory')}: ${(totalMb - freeMb).toFixed(0)}mb (${memPercentString})
-    ${chalk.yellow('CPU Load Average')}: 1-minute: ${chalk.blue(data.osload[0])} 5-minute: ${chalk.blue(data.osload[1])}  15-minute: ${chalk.blue(data.osload[2])}
+    ${chalk.yellow('CPU Load Average')}:
+        1-minute: ${chalk.blue(data.osload[0])}
+        5-minute: ${chalk.blue(data.osload[1])}
+        15-minute: ${chalk.blue(data.osload[2])}
     `);
+    // warn if memory consumption exceeds the expected threshold:
     if (memPercent > options.memoryThreshold) {
-      server.log(['threshold'], chalk.red(`WARNING: Memory use of ${chalk.blue(memPercent)}% exceeds threshold of ${chalk.blue(options.threshold)}%`));
+      server.log(['ops', 'threshold'], chalk.red(`WARNING: MEMORY USE OF ${memPercent}% EXCEEDS THRESHOLD OF ${options.memoryThreshold}%!!!!`));
     }
-    // todo: only print requests if needed:
-    server.log(['requests'], {
-      requests: data.requests,
-      response: data.responseTimes
+    // warn if any of the CPU load averages exceeds the expected threshold:
+    for (var i = 0; i < 3; i++) {
+      const cpuLabel = ['1-MINUTE', '5-MINUTE', '15-MINUTE'][i];
+      const cpuThreshold = options.cpuThresholds[i];
+      const cpuAvg = data.osload[i];
+      if (cpuAvg > cpuThreshold) {
+        server.log(['ops', 'threshold'], chalk.red(`WARNING: AVERAGE ${cpuLabel} CPU LOAD OF ${cpuAvg} EXCEEDS THRESHOLD OF ${cpuThreshold}`));
+      }
+    }
+    // log request info:
+    server.log(['ops', 'requests'], {
+      requests: data.requests[server.info.port],
     });
-    // if (options.memoryThreshold) {
-    //   osload.forEach((osUse) => {
-    //     if (osUse > memoryThreshold) {
-    //       server.log(['threshold'], osUse);
-    //     }
-    //   });
-    // }
+    // track % of requests that were not HTTP OK:
+    if (data.requests[server.info.port] !== undefined) {
+      const requestData = data.requests[server.info.port];
+      if (requestData.total) {
+        totalRequestsProcessed += requestData.total;
+        Object.keys(requestData.statusCodes).forEach((code) => {
+          if (code !== '200') {
+            totalErrorRequests += requestData.statusCodes[code];
+          }
+        });
+      }
+      const failedRequestPercent = ((totalErrorRequests / totalRequestsProcessed) * 100).toFixed(2);
+      if (failedRequestPercent > options.failedRequestThreshold) {
+        server.log(['ops', 'threshold'], chalk.red(`WARNING: ${failedRequestPercent}% OF HTTP REQUESTS HAVE RETURNED A NON-200 CODE`));
+      }
+    }
   });
+  server.expose('oppsy', oppsy);
   server.ext('onPostStart', (server2, next2) => {
     oppsy.start(interval);
     next2();
