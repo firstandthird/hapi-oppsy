33b7512f4cb1adfee5b7bc59ab6a3c0804598a02
orthagonal <orthagonal1@gmail.com>
2017-04-17 09:02:18 -0500

    add example, update main

---
+++ example/index.js
@@ -0,0 +1,20 @@
+const Hapi = require('hapi');
+const plugin = require('../');
+const server = new Hapi.Server({ debug: { log: ['ops', 'cpu', 'requests'] } });
+server.connection({ port: 8080 });
+server.register({
+  register: plugin,
+  options: {
+    interval: 2000
+  }
+}, () => {
+  server.route({
+    path: '/',
+    method: 'GET',
+    handler(req, reply) {
+      console.log('route called');
+      reply('hi');
+    }
+  });
+  server.start(() => {});
+});
--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 const Oppsy = require('oppsy');
+const defaults = {
+  cpu: true,
+  requests: true,
+  httpErrorRequestThreshold: false,
+  memoryThreshold: 0
+};
+
 exports.register = function(server, options, next) {
+  const port = server.info.port;
+  options = Object.assign(defaults, options);
   const interval = options.interval || 1000 * 15;
   const oppsy = new Oppsy(server);
   oppsy.on('ops', (data) => {
-    server.log(['ops'], data);
+    // count # of HTTP errs per report:
+    let httpErrorRequestCount = 0;
+    if (data.requests[port]) {
+      Object.keys(data.requests[port].statusCodes).forEach((code) => {
+        if (code !== '200') {
+          httpErrorRequestCount++;
+        }
+      });
+    }
+    // log cpu/mem stats:
+    server.log(['ops', 'cpu'], {
+      total: data.osmem.total,
+      free: data.osmem.free,
+      osmem_free: `${ (data.osmem.total / data.osmem.free).toFixed(2) }%`,
+    });
+    server.log(['requests'], {
+      requests: data.requests,
+      response: data.responseTimes
+    });
+    if (options.memoryThreshold) {
+      osload.forEach((osUse) => {
+        if (osUse > memoryThreshold) {
+          server.log(['threshold'], osUse);
+        }
+      });
+    }
   });
-
   server.ext('onPostStart', (server2, next2) => {
     oppsy.start(interval);
     next2();
